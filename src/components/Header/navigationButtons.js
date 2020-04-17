dd5b424118ec297c5973c8707352b316a2daca4f
kurt@easil.com <kurt@easil.com>
2020-04-17 16:07:21 +1000

    #comment broke down the structure for the header into easily manageable and reusable parts along with applying modular css principles

---
+++ src/components/Header/navigationButtons.js
@@ -0,0 +1,16 @@
+export const navigationButtons = [
+  {
+    title: "Blog",
+    to: "/blog"
+  },
+  {
+    title: "Addons",
+    to: "/recommended-addons"
+  },
+  {
+    title: "About Killerbyte",
+    to: "/about"
+  }
+];
+
+export default navigationButtons;

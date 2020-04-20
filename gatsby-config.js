bbe1e44aa8b9b5d4e425be5655a88bc6af5f3307
kurt@easil.com <kurt@easil.com>
2020-04-20 14:26:47 +1000

    #comment added LifeCraft font to static font css

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,16 @@ module.exports = {
           }
         ]
       }
-    }
+    },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        custom: {
+          families: ["LifeCraft"],
+          urls: ["/fonts/fonts.css"],
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
